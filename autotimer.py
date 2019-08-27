bd738e466dc5ba38d6215013628ffab73079d3ca
Pedro Cutiño <pedro.cutino23@gmail.com>
2019-08-27 17:01:54 -0500

    ported to windows

--- autotimer.py
+++ autotimer.py
@@ -1,22 +1,31 @@
 from __future__ import print_function
-from AppKit import NSWorkspace
+# from AppKit import NSWorkspace
 import time
-from Foundation import *
+# from Foundation import *
 from os import system
 from activity import *
 import json
 import datetime
 
+import pywintypes
+import win32gui
+import win32con
+
+import os
+
+project_file_path = r"D:\Development\Scripts\AutoTimer"
 active_window_name = ""
 activity_name = ""
 start_time = datetime.datetime.now()
-activeList = AcitivyList([]) 
+activeList = ActivityList([])
 first_time = True
+os.chdir(project_file_path)
 
 
 def url_to_name(url):
     string_list = url.split('/')
     return string_list[2]
+
 
 try:
     activeList.initialize_me()
@@ -27,42 +36,43 @@ except Exception:
 try:
     while True:
         previous_site = ""
-        new_window_name = (NSWorkspace.sharedWorkspace()
-        .activeApplication()['NSApplicationName'])
-        
-        if new_window_name == 'Google Chrome':
-            textOfMyScript = """tell app "google chrome" to get the url of the active tab of window 1"""
-            s = NSAppleScript.initWithSource_(NSAppleScript.alloc(), textOfMyScript)
-            results, err = s.executeAndReturnError_(None)
-            new_window_name = url_to_name(results.stringValue())
+
+        window_code = win32gui.GetForegroundWindow()
+        window_text = win32gui.GetWindowText(window_code)
+        print('Window Text:{}'.format(window_text))
+
+        window_text_array = window_text.split(' - ')
+        new_window_name = window_text_array[-1]
+
+        if new_window_name == 'Google Chrome' or new_window_name == 'Brave' or new_window_name == 'Opera':
+            new_window_name = window_text_array[-2]
 
         if active_window_name != new_window_name:
-            print(active_window_name)
+            print(new_window_name)
             activity_name = active_window_name
-            
+
             if not first_time:
                 end_time = datetime.datetime.now()
                 time_entry = TimeEntry(start_time, end_time, 0, 0, 0, 0)
                 time_entry._get_specific_times()
-            
+
                 exists = False
                 for activity in activeList.activities:
                     if activity.name == activity_name:
                         exists = True
                         activity.time_entries.append(time_entry)
-            
+
                 if not exists:
                     activity = Activity(activity_name, [time_entry])
-                    activeList.activities.append(activity)  
-                with open('activities.json', 'w') as json_file:  
-                    json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
+                    activeList.activities.append(activity)
+                with open('{}\\activities\\{}-{}-{}.json'.format(os.getcwd(), datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year), 'w') as json_file:
+                    json.dump(activeList.serialize(), json_file,
+                              indent=4, sort_keys=True)
                     start_time = datetime.datetime.now()
             first_time = False
             active_window_name = new_window_name
 
-
         time.sleep(10)
 except KeyboardInterrupt:
-    with open('activities.json', 'w') as json_file:  
+    with open('{}\\activities\\{}-{}-{}.json'.format(os.getcwd(), datetime.datetime.now().day, datetime.datetime.now().month, datetime.datetime.now().year), 'w') as json_file:
         json.dump(activeList.serialize(), json_file, indent=4, sort_keys=True)
-
